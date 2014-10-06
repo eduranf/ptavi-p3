54d205384a536eeb65648a4f8849f947dcbdbf38
miguel angel fernandez sanchez <maferna@iota32.aulas.gsyc.es>
2014-10-06 16:00:18 +0200

    Funcion get_tags ok (lista de diccionarios, con diccionarios de atributos dentro)- agrreglado print get_tags

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -15,71 +15,52 @@ class SmallSMILHandler(ContentHandler):
         Constructor
         """
         self.lista_elem = []
-        self.lista_RL = []
-        self.lista_region = []
-        self.lista_img = []
-        self.lista_audio = []
-        self.lista_txt = []
-        self.rl_width = ""
-        self.rl_height = ""
-        self.rl_bgcolor = ""
-        self.reg_id = ""
-        self.reg_top = ""
-        self.reg_bottom = ""
-        self.reg_left = ""
-        self.reg_right = ""
-        self.img_src = ""
-        self.img_region = ""
-        self.img_begin = ""
-        self.img_dur = ""
-        self.audio_src = ""
-        self.audio_begin = ""
-        self.audio_dur = ""
-        self.txtstr_src = ""
-        self.txtstr_region = ""
-        self.inRL = 0
-        self.inRegion = 0
-        self.inImg = 0
-        self.inAudio = 0
-        self.inTxtstr = 0
+        self.dicc_RLup = {}
+        self.dicc_RL = {}
+        self.dicc_region = {}
+        self.dicc_img = {}
+        self.dicc_audio = {}
+        self.dicc_txtstr = {}
+        self.dicc_regionup = {}
+        self.dicc_imgup = {}
+        self.dicc_audioup = {}
+        self.dicc_txtstrup = {}
 
     def startElement(self, name, attrs):
         """
         Método que se llama cuando se abre una etiqueta
         """
         if name == 'root-layout':
+            self.dicc_RL['width'] = attrs.get('width', "")
+            self.dicc_RL['height'] = attrs.get('height', "")
+            self.dicc_RL['background-color'] = attrs.get('background-color', "")
+            self.dicc_RLup['root-layout'] = self.dicc_RL
 
-            self.rl_width = attrs.get('width', "")
-            self.rl_height = attrs.get('height', "")
-            self.rl_bgcolor = attrs.get('background-color', "")
-            self.lista_RL = [self.rl_width, self.rl_height, self.rl_bgcolor]
-            self.inRL = 1
         elif name == 'region':
-            self.reg_id = attrs.get('id', "")
-            self.reg_top = attrs.get('top', "")
-            self.reg_bottom = attrs.get('bottom', "")
-            self.reg_left = attrs.get('left', "")
-            self.reg_right = attrs.get('right', "")
-            self.lista_region = [self.reg_id, self.reg_top, self.reg_bottom, self.reg_left, self.reg_right]
-            self.inRegion = 1
+            self.dicc_region['id'] = attrs.get('id', "")
+            self.dicc_region['top'] = attrs.get('top', "")
+            self.dicc_region['bottom'] = attrs.get('bottom', "")
+            self.dicc_region['left'] = attrs.get('left', "")
+            self.dicc_region['right'] = attrs.get('right', "")
+            self.dicc_regionup['region'] = self.dicc_region
+
         elif name == 'img':
-            self.img_src = attrs.get('src', "")
-            self.img_region = attrs.get('region', "")
-            self.img_begin = attrs.get('begin', "")
-            self.img_dur = attrs.get('dur', "")
-            self.lista_img = [self.img_src, self.img_region, self.img_begin, self.img_dur, self.inImg]
-            self.inImg = 1
+            self.dicc_img['src'] = attrs.get('src', "")
+            self.dicc_img['region'] = attrs.get('region', "")
+            self.dicc_img['begin'] = attrs.get('begin', "")
+            self.dicc_img['dur'] = attrs.get('dur', "")
+            self.dicc_imgup['img'] = self.dicc_img
+
         elif name == 'audio':
-            self.audio_src = attrs.get('src', "")
-            self.audio_begin = attrs.get('begin', "")
-            self.audio_dur = attrs.get('dur', "")
-            self.lista_audio = [self.audio_src, self.audio_begin, self.audio_dur]
-            self.inAudio = 1
+            self.dicc_audio['src'] = attrs.get('src', "")
+            self.dicc_audio['begin'] = attrs.get('begin', "")
+            self.dicc_audio['dur'] = attrs.get('dur', "")
+            self.dicc_audioup['audio'] = self.dicc_audio
+
         elif name == 'textstream':
-            self.txtstr_src = attrs.get('src', "")
-            self.txtstr_region = attrs.get('region', "")
-            self.lista_txtstr = [self.txtstr_src, self.txtstr_region]
-            self.inTxtstr = 1
+            self.dicc_txtstr['src'] = attrs.get('src', "")
+            self.dicc_txtstr['region'] = attrs.get('region', "")
+            self.dicc_txtstrup['textstream'] = self.dicc_txtstr
 
     def endElement(self, name):
         """
@@ -117,7 +98,7 @@ class SmallSMILHandler(ContentHandler):
         """
         Metodo que devuelve una lista de elementos
         """
-        self.lista_elem = [self.lista_RL, self.lista_region, self.lista_img, self.lista_audio, self.lista_txtstr]
+        self.lista_elem = [self.dicc_RLup, self.dicc_regionup, self.dicc_imgup, self.dicc_audioup, self.dicc_txtstrup]
         return self.lista_elem
     
 
