4bbc650d08e1fec34af35e85fb9e7e67c6ebd12b
mghfdez <mafesan.nsn@gmail.com>
2014-10-10 21:33:06 +0200

    Añadida opcion URL (what-to-do-with-it?) Pendiente: quitar trazas en print

--- karaoke.py
+++ karaoke.py
@@ -2,6 +2,7 @@
 # -*- coding: iso-8859-15 -*-
 
 import sys
+import os
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
@@ -14,15 +15,22 @@ if __name__ == "__main__":
     if len(lista) != 2:
         print "Usage: python karaoke.py file.smil"
     else:
-        fich = lista[1]
-        parser = make_parser()
-        KHandler = SmallSMILHandler()
-        parser.setContentHandler(KHandler)
-        parser.parse(open(fich))
-        lista = KHandler.get_tags()
-        for atributos in lista:
-            dicc = atributos[1]
-            frase = ""
-            for elemento in dicc.keys():
-                frase = frase + elemento + '="' + dicc[elemento] + '"' + "\t"
-            print "Elemento: " + atributos[0] + "\t" + "Atributos: " + frase
+        parametro = lista[1]
+        if parametro.split(':')[0] == "http":
+            parametro = parametro.split("/")[-1]
+            os.system("wget -q " + parametro)
+            print str(parametro)
+            
+        else:
+            fich = lista[1]
+            parser = make_parser()
+            KHandler = SmallSMILHandler()
+            parser.setContentHandler(KHandler)
+            parser.parse(open(fich))
+            lista = KHandler.get_tags()
+            for atributos in lista:
+                dicc = atributos[1]
+                frase = ""
+                for elemento in dicc.keys():
+                    frase = frase + elemento + '="' + dicc[elemento] + '"' + "\t"
+                print "Elemento: " + atributos[0] + "\t" + "Atributos: " + frase
